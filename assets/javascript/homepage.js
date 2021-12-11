c81e01883aa97b2f9d0fda012ead1491cabfc851
moonryc <70920813+moonryc@users.noreply.github.com>
2021-12-11 16:02:25 -0600

    can now retreive user repos

---
+++ assets/javascript/homepage.js
@@ -0,0 +1,87 @@
+let userFormEl = document.querySelector("#user-form")
+let nameInputEl = document.querySelector("#username")
+let repoContainerEl = document.querySelector("#repos-container");
+let repoSearchTerm = document.querySelector("#repo-search-term");
+
+const getUserRepos = (user) => {
+
+    let apiUrl = `https://api.github.com/users/${user}/repos`;
+
+    fetch(apiUrl).then((response) => {
+        if(response.ok){
+            response.json().then((data) => {
+                displayRepos(data, user)
+            })
+        }else{
+            alert("error github user not found")
+        }
+
+    }).catch((error)=> {
+        // Notice this `.catch()` getting chained onto the end of the `.then()` method
+        alert("Unable to connect to GitHub");
+    });
+
+
+}
+
+
+const displayRepos = (repos, searchTerm) => {
+
+    repoContainerEl.textContent = ""
+    repoSearchTerm.textContent = searchTerm
+
+    if(repos.length === 0){
+        repoContainerEl.textContent = "No Repositories were found"
+    }
+
+    for (let i = 0; i < repos.length; i++) {
+        //format repo name e.g. moonryc/reponame
+        let repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        // create a container for each repo
+        let repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        // create a span element to hold repository name
+        let titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        // append to container
+        repoEl.appendChild(titleEl);
+
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        // check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML =
+                "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+
+        // append to container
+        repoEl.appendChild(statusEl);
+
+        // append container to the dom
+        repoContainerEl.appendChild(repoEl);
+    }
+
+}
+
+const formSubmitHandler = (event) => {
+    event.preventDefault()
+    let userName = nameInputEl.value.trim()
+
+    if (userName) {
+        getUserRepos(userName)
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a github username")
+    }
+}
+
+userFormEl.addEventListener("submit", formSubmitHandler)
+
+// getUserRepos("moonryc")
